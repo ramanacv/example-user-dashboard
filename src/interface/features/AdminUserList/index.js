5ea797243a53bba9189cc5d83305b377f480d098
Kames <info@kamescg.com>
2018-05-24 10:15:18 -0400

    add more demo material. getting closer :)

--- src/interface/features/AdminUserList/index.js
+++ src/interface/features/AdminUserList/index.js
@@ -1,5 +1,6 @@
 /* ------------------------- External Dependencies -------------------------- */
 import _ from 'lodash'
+import idx from './idx'
 import React from 'react';
 import { compose, lifecycle, withProps, withState, withHandlers, renderComponent } from 'recompose'
 import { connect } from 'react-redux'
@@ -49,16 +50,32 @@ const ComponentRender = props => <div>
   {
     !props.data ? null :
     Object.keys(props.data).map(item => <Box>
-      <Flex align='center' boxShadow={0} bg='grayLight' p={10} w={1} >
-        <Box  w={[1,0.2]}>
-          <Heading f={[3]}  >
-            {props.data[item].name}
-          </Heading>
+      <Flex align='center' boxShadow={0} bg='grayLight' p={10} w={1} mb={15} >
+        <Flex  w={[1,0.2]} align='center' >
+        <Box
+          borderRadius={9999999}
+          bc="white"
+          b="2px solid #FFF"
+          boxShadow={2}
+          of="hidden"
+          mt={0}
+          ml="auto"
+          mr="auto"
+          h={100}
+          w={100}
+        >
+          <BackgroundImage 
+            src={idx(props, _=>_.data[item].profile.avatar.uri)}
+          />
         </Box>
+          <Heading f={[3]}  >
+            {props.data[item].profile.name}
+          </Heading>
+        </Flex>
         <Flex w={[1,0.6]} justify='space-evenly' >
-          <Span>{props.data[item].email}</Span>
-          <Span>{props.data[item].phone}</Span>
-          <Span>{props.data[item].country}</Span>
+          <Span>{props.data[item].profile.email}</Span>
+          <Span>{props.data[item].profile.phone}</Span>
+          <Span>{props.data[item].profile.country}</Span>
         </Flex>
         <Flex w={[1,0.2]} >
           <Button>Request Attestation</Button>
--- src/interface/features/uport/UPortAttestAddressForm/form/index.js
+++ src/interface/features/uport/UPortAttestAddressForm/form/index.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Field } from 'redux-form'
 import { QRCode } from 'react-qr-svg'
-import idx from './idx'
+// import idx from './idx'
 /* ------------------------- Internal Dependencies -------------------------- */
 import { Box, Flex, Button, Heading, ReduxField } from 'atomic'
 import Form from 'molecules/Form'
@@ -11,26 +11,6 @@ import DialogOpen from 'containers/dialog/DialogOpen'
 /* ---------------------------- Form Component ------------------------------ */
 export default ({ handleSubmit, isSubmitting, styled, ...props}) => (
 <Form {...styled}>
-<Flex bg={'white'} align="center" justify="center" p={20} >
-  {
-    !idx(props, _=>_.attestationRequest.admin.issued) ? null : 
-    <Box>
-      <DialogOpen foundry='WrapperMeetupEvent'>
-        <Button gradient='purple' w={1} >
-          Meetup RSVP
-        </Button>
-      </DialogOpen>
-      {/* <QRCode
-        bgColor="#FFFFFF"
-        fgColor="#000000"
-        level="Q"
-        style={{ width: 400 }}
-        value={props.attestationRequest.admin.attestation}
-      />  */}
-    </Box>
-  }
-
-</Flex>
   <Heading level={[3]} f={[3]}>
     Address Attestation
   </Heading>
